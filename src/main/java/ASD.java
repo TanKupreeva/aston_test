a3df528582165c145907796875532e50bd9d4e6e
Tatyana Kupreeva <>
2025-01-30 18:44:35 +0300

    tom2

--- src/main/java/ASD.java
+++ src/main/java/ASD.java
@@ -1,7 +1,7 @@
 public class ASD {
     public static void main(String[] args) {
         System.out.println("Bye");
-        System.out.println("Hello TOM!");
+        System.out.println("Hello TOM");
 
     }
 }
